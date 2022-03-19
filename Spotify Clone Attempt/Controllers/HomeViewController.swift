ccf18aa0bfab82727a5fbc6be719815e0a067b40
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2022-03-19 15:56:31 +0200

    added recents collection view

--- Spotify Clone Attempt/Controllers/HomeViewController.swift
+++ Spotify Clone Attempt/Controllers/HomeViewController.swift
@@ -9,12 +9,33 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var recentsCollectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        let cellNib = UINib(nibName: "RecentsCollectionViewCell", bundle: nil)
 
+        self.recentsCollectionView.register(cellNib, forCellWithReuseIdentifier: "recent")
+        recentsCollectionView.dataSource = self
+    }
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
     }
 
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = recentsCollectionView.dequeueReusableCell(withReuseIdentifier: "recent", for: indexPath) as? RecentsCollectionViewCell{
+            cell.playlistCover = UIImageView(image: UIImage(named: "cover"))
+            return cell
+        }
+        return UICollectionViewCell()
+    }
 
 
 }
---
+++ Spotify Clone Attempt/Views/RecentsCollectionView/RecentsCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  RecentsCollectionViewCell.swift
+//  Spotify Clone Attempt
+//
+//  Created by Abdulrahman on 3/19/22.
+//
+
+import UIKit
+
+class RecentsCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var playlistCover: UIImageView!
+    @IBOutlet var playlistLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
