33406a1f250a2e2a43983aab739a44e14c05eca6
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2022-03-24 00:33:12 +0200

    added search bar and collection view

--- Spotify Clone Attempt/Controllers/SearchViewController.swift
+++ Spotify Clone Attempt/Controllers/SearchViewController.swift
@@ -8,18 +8,65 @@
 import UIKit
 
 class SearchViewController: UIViewController, UISearchBarDelegate {
+    var addedFirstHeader: Bool = false
+    var addedSecondHeader: Bool = false
+
+
+    @IBOutlet weak var genresCollectionView: UICollectionView!
 
     @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
 
-
-
-        navigationItem.hidesSearchBarWhenScrolling = false
+        let headerCellNib = UINib(nibName: "GenresCollectionViewHeader", bundle: nil)
+        self.genresCollectionView.register(headerCellNib, forCellWithReuseIdentifier: "header")
     }
 
-    
-    
+
+
+
+}
+
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        2
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        5
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "genre", for: indexPath)
+        cell.backgroundColor = .blue
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let headerCell = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "header", for: indexPath)
+        let headerView = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
+        headerCell.addSubview(headerView)
+
+
+        if indexPath.section == 0 && addedFirstHeader == false {
+            addedFirstHeader = true
+            headerView.text = "Your genres"
+            return headerCell
+        }
+        else if indexPath.section == 2 && addedSecondHeader == false {
+            addedSecondHeader = true
+            headerView.text = "Discover"
+            return headerCell
+        }
+        else {
+            headerView.frame = CGRect.zero
+            return headerCell
+
+        }
+
+
+    }
 
 }
---
+++ Spotify Clone Attempt/Views/GenresCollectionView/GrenresCollectionViewHeader/GenresCollectionViewHeader.swift
@@ -0,0 +1,17 @@
+//
+//  GenresCollectionViewHeader.swift
+//  Spotify Clone Attempt
+//
+//  Created by Abdulrahman on 3/23/22.
+//
+
+import UIKit
+
+class GenresCollectionViewHeader: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
